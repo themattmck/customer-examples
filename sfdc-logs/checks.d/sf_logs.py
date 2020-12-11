19684e8d2d664321d6cee06f2a589a734bdb8dab
Matt McKeown <matt@openorchestrate.com>
2020-12-11 08:48:28 -0500

    changed how logs are accepted

--- sfdc-logs/checks.d/sf_logs.py
+++ sfdc-logs/checks.d/sf_logs.py
@@ -11,21 +11,30 @@ class SFDCLogs(AgentCheck):
         formatted_date_string = last_hour.strftime("%Y-%m-%dT%H:00:00.000+0000")
 
         # Format the URL based on the instance identifier and date
-        sf_url = "https://{}/services/data/v48.0/query".format(instance["url"])
+        base_url = "https://{}".format(instance["url"])
+        sf_url = "{}{}".format(base_url, "asdf")
         query = {"q": "SELECT+Id,EventType,LogDate,CreatedDate+FROM+EventLogFile+WHERE+CreatedDate>={}+AND+Interval='Hourly'+ORDER+BY+CreatedDate+LIMIT+1000".format(formatted_date_string)}
         sf_request = requests.get(url=sf_url, params=query, auth=(instance["username"], instance["password"]), headers=[])
         print(sf_request.status_code)
         sf_logs = sf_request.json()["records"]
-        dd_headers = {
-            "DD-API-KEY": instance["api-key"],
-            "Content-Type": "application/json"
-        }
-        dd_url = "https://http-intake.logs.datadoghq.com/v1/input"
         for log in sf_logs:
-            log["ddsource"]=instance["source"]
-            log["service"]=instance["service"]
-            log["ddtags"]="env:{}".format(instance["env"])
-            log["timestamp"]=log["LogDate"].split("+")[0]
-            r = requests.post(url=dd_url,headers=dd_headers,json=log)
-            print(r.status_code)
-            print(log)
+            r = requests.get(url="http://winterolympicsmedals.com/medals.csv")
+            with open("out.csv", 'wb') as fd:
+                for chunk in r.iter_content(chunk_size=128):
+                    fd.write(chunk)
+            with open('out.csv', newline='\n') as csvfile:
+                keys = []
+                line_count = 0
+                for row in csvfile:
+                    if line_count == 0:
+                        for key in row.split(","):
+                            keys.append(key.strip("\n"))
+                    else:
+                        json_payload = {}
+                        values = row.split(",")
+                        for value in values:
+                            json_payload[keys[values.index(value)]]=value.strip("\n")
+                        print(json_payload)
+                        with open('log.txt', 'a') as log_file:
+                            log_file.write("{}\n".format(json_payload))
+                    line_count += 1
